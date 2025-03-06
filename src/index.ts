81a00750b8c3fca5d0607dc7ba9b951c3727f303
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-06 18:37:45 +0000

    Release 0.0.4

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export * as Tesseral from "./api";
 export { TesseralClient } from "./Client";
 export { TesseralEnvironment } from "./environments";
 export { TesseralError, TesseralTimeoutError } from "./errors";
-export { TesseralAuthenticator, InvalidAccessTokenError } from "./authenticate";
