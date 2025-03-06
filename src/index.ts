054da4cfbad150413d796383b3d3601b744d2c16
Ulysse Carion <ulysse.carion@ssoready.com>
2025-03-06 10:43:49 -0800

    Fix .fernignore, restore changes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * as Tesseral from "./api";
 export { TesseralClient } from "./Client";
 export { TesseralEnvironment } from "./environments";
 export { TesseralError, TesseralTimeoutError } from "./errors";
+export { TesseralAuthenticator, InvalidAccessTokenError } from "./authenticate";
