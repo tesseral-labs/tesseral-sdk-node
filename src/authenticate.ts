e06e19efdcd1f72402f5a120a8a39a15150fb786
Ulysse Carion <ulysse.carion@ssoready.com>
2025-04-03 11:07:05 -0700

    Rework AccessTokenAuthenticator to expose getProjectId()

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -1,26 +1,27 @@
 import * as crypto from "crypto";
 import { AccessTokenClaims } from "./api";
 
-export class TesseralAuthenticator {
-    #publishable_key: string;
-    #config_api_host: string;
-    #jwks?: Record<string, crypto.KeyObject>;
-    #jwks_next_refresh_unix_seconds: number;
-    #jwks_refresh_interval_seconds: number;
+export class AccessTokenAuthenticator {
+    private publishableKey: string;
+    private configApiHostname: string;
+    private projectId?: string;
+    private jwks?: Record<string, crypto.KeyObject>;
+    private jwksNextRefreshUnixSeconds: number;
+    private jwksRefreshIntervalSeconds: number;
 
     constructor({
         publishableKey,
-        configApiHost = "config.tesseral.com",
+        configApiHostname = "config.tesseral.com",
         jwksRefreshIntervalSeconds = 3600,
     }: {
         publishableKey: string;
-        configApiHost?: string;
+        configApiHostname?: string;
         jwksRefreshIntervalSeconds?: number;
     }) {
-        this.#publishable_key = publishableKey;
-        this.#config_api_host = configApiHost;
-        this.#jwks_refresh_interval_seconds = jwksRefreshIntervalSeconds;
-        this.#jwks_next_refresh_unix_seconds = 0;
+        this.publishableKey = publishableKey;
+        this.configApiHostname = configApiHostname;
+        this.jwksRefreshIntervalSeconds = jwksRefreshIntervalSeconds;
+        this.jwksNextRefreshUnixSeconds = 0;
     }
 
     public async authenticateAccessToken({
@@ -30,59 +31,68 @@ export class TesseralAuthenticator {
         accessToken: string;
         nowUnixSeconds?: number;
     }): Promise<AccessTokenClaims> {
-        if (!this.#jwks || Date.now() / 1000 > this.#jwks_next_refresh_unix_seconds) {
-            this.#jwks = await this.#fetchJwks();
-            this.#jwks_next_refresh_unix_seconds = Date.now() / 1000 + this.#jwks_refresh_interval_seconds;
-        }
-
+        await this.updateConfigData();
         return authenticateAccessToken({
-            jwks: this.#jwks,
+            jwks: this.jwks!,
             accessToken,
             nowUnixSeconds: nowUnixSeconds ?? Date.now() / 1000,
         });
     }
 
-    async #fetchJwks(): Promise<Record<string, crypto.KeyObject>> {
-        const response = await fetch(`https://${this.#config_api_host}/v1/config/${this.#publishable_key}`);
+    public async getProjectId(): Promise<string> {
+        await this.updateConfigData();
+        return this.projectId!;
+    }
+
+    private async updateConfigData(): Promise<void> {
+        if (Date.now() / 1000 < this.jwksNextRefreshUnixSeconds) {
+            return;
+        }
+
+        const response = await fetch(`https://${this.configApiHostname}/v1/config/${this.publishableKey}`);
         if (!response.ok) {
             throw new Error("Failed to fetch JWKS");
         }
-        return parseJwks(await response.json());
+        const { projectId, jwks } = parseConfig(await response.json());
+        this.projectId = projectId;
+        this.jwks = jwks;
+        this.jwksNextRefreshUnixSeconds = Date.now() / 1000 + this.jwksRefreshIntervalSeconds;
     }
 }
 
-/**
- * Not for public use. Instead, use a TesseralAuthenticator instance
- * directly.
- *
- * This method is exported only to enable unit testing TesseralAuthenticator's
- * internals.
- */
-export function authenticateAccessToken({ jwks, accessToken, nowUnixSeconds }: { jwks: Record<string, crypto.KeyObject>, accessToken: string, nowUnixSeconds: number }): AccessTokenClaims {
-    const parts = accessToken.split(".")
+function authenticateAccessToken({
+    jwks,
+    accessToken,
+    nowUnixSeconds,
+}: {
+    jwks: Record<string, crypto.KeyObject>;
+    accessToken: string;
+    nowUnixSeconds: number;
+}): AccessTokenClaims {
+    const parts = accessToken.split(".");
     if (parts.length !== 3) {
         throw new InvalidAccessTokenError();
     }
 
     const [rawHeader, rawClaims, rawSignature] = parts;
 
-    const parsedHeader = JSON.parse(base64URLDecode(parts[0]))
+    const parsedHeader = JSON.parse(base64URLDecode(parts[0]));
     if (!(parsedHeader.kid in jwks)) {
         throw new InvalidAccessTokenError();
     }
 
     const publicKey = jwks[parsedHeader.kid];
 
-    const signature = Buffer.from(rawSignature.replace(/-/g, "+").replace(/_/g, "/"), 'base64')
+    const signature = Buffer.from(rawSignature.replace(/-/g, "+").replace(/_/g, "/"), "base64");
     const valid = crypto.verify(
-        'sha256',
-        Buffer.from(rawHeader + '.' + rawClaims),
+        "sha256",
+        Buffer.from(rawHeader + "." + rawClaims),
         {
             key: publicKey,
-            dsaEncoding: 'ieee-p1363',
+            dsaEncoding: "ieee-p1363",
         },
         signature
-    )
+    );
     if (!valid) {
         throw new InvalidAccessTokenError();
     }
@@ -96,35 +106,38 @@ export function authenticateAccessToken({ jwks, accessToken, nowUnixSeconds }: {
 }
 
 function base64URLDecode(s: string): string {
-    return Buffer.from(s.replace(/-/g, "+").replace(/_/g, "/"), 'base64').toString();
+    return Buffer.from(s.replace(/-/g, "+").replace(/_/g, "/"), "base64").toString();
 }
 
-/**
- * Not for public use. Instead, use a TesseralAuthenticator instance
- * directly.
- *
- * This method is exported only to enable unit testing TesseralAuthenticator's
- * internals.
- */
-export function parseJwks(jwksData: any): Record<string, crypto.KeyObject> {
-    const jwks: Record<string, crypto.KeyObject> = {}
-    for (const key of jwksData.keys) {
-        if (key.kty !== 'EC' || key.crv !== 'P-256') {
-            throw new Error("internal error: jwks must contain P-256 elliptic public keys")
+interface Config {
+    projectId: string;
+    jwks: Record<string, crypto.KeyObject>;
+}
+
+function parseConfig(configData: any): Config {
+    const jwks: Record<string, crypto.KeyObject> = {};
+    for (const key of configData.keys) {
+        if (key.kty !== "EC" || key.crv !== "P-256") {
+            throw new Error("internal error: jwks must contain P-256 elliptic public keys");
         }
 
         jwks[key.kid] = crypto.createPublicKey({
-            format: 'jwk',
+            format: "jwk",
             key,
-        })
+        });
     }
 
-    return jwks;
+    return { projectId: configData.projectId, jwks };
 }
 
-export class InvalidAccessTokenError extends Error {
+class InvalidAccessTokenError extends Error {
     constructor() {
         super("Invalid access token");
         Object.setPrototypeOf(this, InvalidAccessTokenError.prototype);
     }
 }
+
+export const __exportedForTests = {
+    parseConfig,
+    authenticateAccessToken,
+};
